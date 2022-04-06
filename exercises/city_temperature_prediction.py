22c01dd2362e54176a18b2e6abe48259d4e90cae
eitan lev <eitanleviev111@gmail.com>
2022-04-06 17:51:43 +0300

    ex2 poly - q3

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -27,8 +27,8 @@ def load_data(filename: str) -> pd.DataFrame:
     data = pd.read_csv(filename, parse_dates=True)
 
     # value validation
-    data = data[data["Temp"] > -50]
-    data = data[data["Temp"] < 80]
+    data = data[data["Temp"] > -30]
+    data = data[data["Temp"] < 60]
 
     # new features
     data['DayOfYear'] = pd.to_datetime(data['Date']).dt.dayofyear
@@ -75,13 +75,25 @@ if __name__ == '__main__':
         yaxis_title="temperatures std")
     fig.write_image("../exercises/temp.std.per.month.png")
 
-
-
-    pass # debugging
-
     # Question 3 - Exploring differences between countries
 
+    temp_statistic_df = df.groupby(['Country', 'Month']).Temp.agg([np.mean, np.std])
+    month_indices = temp_statistic_df.index.get_level_values('Month')
+    country_indices = temp_statistic_df.index.get_level_values('Country')
+    fig = px.line(temp_statistic_df,
+                  x=month_indices,
+                  y=temp_statistic_df['mean'],
+                  error_y=temp_statistic_df['std'],
+                  color=country_indices)
+    fig.update_layout(
+        title="mean temp +- std of all countries as a func of month",
+        xaxis_title="month",
+        yaxis_title="mean temp +- std")
+    fig.write_image("../exercises/mean.temp.for.each.coumtry.png")
+
     # Question 4 - Fitting model for different values of `k`
+
+    pass # debugging
 
     # Question 5 - Evaluating fitted model on different countries
 
